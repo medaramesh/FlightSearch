0cb24d68d62c677620264a6fb6a4523a0dde3f5a
medaramesh <79666129+medaramesh@users.noreply.github.com>
2023-10-20 07:38:54 -0400

    Update example2.sh

--- example2.sh
+++ example2.sh
@@ -3,4 +3,5 @@ echo "The content of directory displayed here"
 ls -l
 echo "Change directory"
 cd \etc\ansible
-ansible-playbook unventou.ini playbook.name
+ansible-playbook unventou.ini playbook.yml
+echo "This is code not working"
