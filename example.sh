643c2009e07290397baf731dd60787237b469beb
medaramesh <79666129+medaramesh@users.noreply.github.com>
2023-10-26 07:31:16 -0400

    Update example.sh

--- example.sh
+++ example.sh
@@ -3,4 +3,8 @@ echo "Hello World"
 echo "give me current directory"
 pwd
 echo "Here is another changes"
+<<<<<<< Updated upstream
 echo "create a new branch"
+=======
+echo "create a new branch feature branch for merge conflict"
+>>>>>>> Stashed changes
